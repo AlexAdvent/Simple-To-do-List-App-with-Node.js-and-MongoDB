6922293a88715b0c30b6fcda0f0b8c89b27a68f1
LP-Codes <lovelesh_p@yahoomail.com>
2021-03-18 12:46:51 +0530

    fix moongoose depriciation warning

--- index.js
+++ index.js
@@ -62,7 +62,8 @@ app
 
 //connection to db
 // mongoose.set("useUnifiedTopology: true")
-mongoose.set("useFindAndModify", false);
+// mongoose.set("useFindAndModify", false);
+mongoose.set('useUnifiedTopology', true);
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
   console.log("Connected to db!");
   app.listen(3000, () => console.log("Server Up and running on ${port}"));
